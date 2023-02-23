9d548ff61ccded1dbe04e6e0c4ff96aaf53b7479
manimaran4509744 <akash4509744@gmail.com>
2023-02-23 08:38:42 +0530

    Initial commit

---
+++ src/test/java/org/logger/LoggerPractice.java
@@ -0,0 +1,19 @@
+package org.logger;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LoggerPractice {
+	public static void main(String[] args) {
+		Logger logger = LogManager.getLogger(LoggerPractice.class.getName());
+		logger.info("Object is created for logger class");
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
